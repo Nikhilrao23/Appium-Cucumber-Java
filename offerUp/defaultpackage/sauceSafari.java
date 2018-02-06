319098cce5303145bbfee70c70c8d4d7af3d089f
Nikhil Rao Balaji <raonikhil23@gmail.com>
2018-02-05 19:55:37 -0500

    updated sauceSafari.java
    removed UN and Key generated from my Account

--- offerUp/defaultpackage/sauceSafari.java
+++ offerUp/defaultpackage/sauceSafari.java
@@ -23,7 +23,7 @@ public class sauceSafari {
 		caps.setCapability("platformName", "iOS");
 		caps.setCapability("browserName", "Safari");
 		
-		IOSDriver<IOSElement> driver = new IOSDriver<>(new URL("http://nikbik:46396203-f58d-4711-abdb-64c101ef86b9@ondemand.saucelabs.com:80/wd/hub"),caps);
+		IOSDriver<IOSElement> driver = new IOSDriver<>(new URL("http://username:key@ondemand.saucelabs.com:80/wd/hub"),caps);
 		
 		driver.get("https://www.google.com");
 		Thread.sleep(3000);
