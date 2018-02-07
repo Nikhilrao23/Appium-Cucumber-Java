6854656d34085abde0743e6107863831292d4509
Nikhilrao23 <raonikhil23@gmail.com>
2018-02-07 17:17:22 -0500

    Learning TestNG Framework

---
+++ testNG/src/testngpack/annotations.java
@@ -0,0 +1,19 @@
+package testngpack;
+
+import org.testng.annotations.Test;
+
+public class annotations {
+	
+	@Test
+	// Similar to Public static void main in java class
+	public void OpeningBrowser() {
+		// Creating a method Opening Browser
+	System.out.println("Nikhil Rao");
+	
+	}
+	
+	@Test
+	public void bookFlight() {
+		System.out.println("Flight Booking");
+	}
+}
