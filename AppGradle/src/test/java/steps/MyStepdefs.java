2ed6ad32e5657f13551b66ef676a5e6cb058b1b0
Nikhilrao23 <raonikhil23@gmail.com>
2018-02-19 22:00:21 -0500

    Adding Cucumber logic to Appium Test Scripts

--- AppGradle/src/test/java/steps/MyStepdefs.java
+++ AppGradle/src/test/java/steps/MyStepdefs.java
@@ -1,6 +1,9 @@
 package steps;
 import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -8,19 +11,49 @@ import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 public class MyStepdefs {
+    private AppiumDriver driver;
+
     @When("^I launch OfferUp app$")
     public void iLaunchOfferUpApp() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         DesiredCapabilities cb = new DesiredCapabilities();
         cb.setCapability(MobileCapabilityType.DEVICE_NAME, "Nexus");
         cb.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-        cb.setCapability(AndroidMobileCapabilityType.APP_PACKAGE,"com.offerup");
-        cb.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY,"com.offerup.android.search.SearchActivity");
+        cb.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.offerup");
+        cb.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "com.offerup.android.search.SearchActivity");
 
-        AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"),cb);
+        driver = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub"), cb);
 
-        driver.findElementByAndroidUIAutomator("new UiSelector().text(\"ALLOW\")").click();
+        Thread.sleep(3000);
+    }
+
+    @And("^I chose Allow on Location Services$")
+    public void iChoseAllowOnLocationServices() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("Click on Allow Button");
+        driver.findElementByXPath("//android.widget.Button[@text = 'ALLOW']").click();
+    }
+
+    @And("^Click on Account on Bottom Navigational Panel$")
+    public void clickOnAccountOnBottomNavigationalPanel() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        driver.findElementById("com.offerup:id/nav_profile").click();
+
+    }
+
+    @And("^Tap on Continue with Google$")
+    public void tapOnContinueWithGoogle() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        driver.findElementById("com.offerup:id/google_signin_button").click();
+    }
+
+    @Then("^I see my existing google account \"([^\"]*)\"$")
+    public void iSeeMyExistingGoogleAccount(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        driver.findElementById("com.google.android.gms:id/account_display_name").click();
+        System.out.println(driver.findElementByXPath("//android.widget.TextView[@index = '4']"));
     }
 }
--- offerUp/defaultpackage/github.java
+++ offerUp/defaultpackage/github.java
@@ -4,14 +4,22 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
 
 public class github {
+	public static AndroidDriver<AndroidElement> driver;
+	// public static AppiumDriver<?> driver1;
+	@FindBy(id = "jp.forkhub:id/et_login") private static MobileElement userName;
 	public static void main(String[] args) {
 		
 		try {
@@ -25,12 +33,11 @@ public class github {
 
 	public static void logIn() {
 		// TODO Auto-generated method stub
-		
-		
-		
+		System.out.println("Log In Page");
+		userName.sendKeys("Nikhil");
 	}
 
-	public static void appiumDriver() throws MalformedURLException {
+	private static void appiumDriver() throws MalformedURLException {
 		// TODO Auto-generated method stub
 		
 		DesiredCapabilities ty = new DesiredCapabilities();
@@ -38,11 +45,26 @@ public class github {
 		ty.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
 		ty.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "jp.forkhub");
 		ty.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "com.github.mobile.ui.user.HomeActivity");
-		AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"),ty);
+		AndroidDriver AndroidDriver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"),ty);
+		driver = AndroidDriver;
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		System.out.println("Appium Driver Started");
-		
+	
 	}
+
+	/*
+	public static void logIn() {
+		// TODO Auto-generated method stub
+		
+	if(driver.findElements(By.id("jp.forkhub:id/et_login")).size() >= 0){
+		System.out.println("Entering Username");
+		userName.sendKeys("nikhilraoqaintern");
 	
+		System.out.println("UserName");
+		driver.hideKeyboard();
+		System.out.println("Hide Keyboard");
+		userName.sendKeys("Nikhil");
 	
-}
+	}
+	*/
+	}
